36127b29c1c1e977cf8f335a44c2dfc8bc5fffd8
Györfi Marcell <44555742+gyorfimarcell@users.noreply.github.com>
2023-06-01 10:54:19 +0200

    Add files via upload

---
+++ Dij.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Dij
+    {
+        int evszam;
+        string tipus;
+        string keresztnev;
+        string vezeteknev;
+
+        public Dij(string csvSor)
+        { 
+            string[] mezok = csvSor.Split(';');
+            this.evszam = Convert.ToInt32(mezok[0]);
+            this.tipus = mezok[1];
+            this.keresztnev = mezok[2];
+            this.vezeteknev = mezok[3];
+        }
+
+        public Dij(int evszam, string tipus, string keresztnev, string vezeteknev)
+        {
+            this.evszam = evszam;
+            this.tipus = tipus;
+            this.keresztnev = keresztnev;
+            this.vezeteknev = vezeteknev;
+        }
+
+        public int Evszam { get => evszam; }
+        public string Tipus { get => tipus; }
+        public string Keresztnev { get => keresztnev; }
+        public string Vezeteknev { get => vezeteknev; }
+
+        public string Nev { get => $"{keresztnev} {vezeteknev}"; }
+        public bool Szervezet { get => vezeteknev == ""; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dij> dijak = File.ReadAllLines("nobel.csv").Skip(1).Select(x => new Dij(x)).ToList();
+
+            Console.WriteLine($"3. feladat: {dijak.Find(x => x.Nev == "Arthur B. McDonald").Tipus}");
+
+            Console.WriteLine($"4. feladat: {dijak.Find(x => x.Evszam == 2017 && x.Tipus == "irodalmi").Nev}");
+
+            Console.WriteLine("5. feladat:");
+            dijak.Where(x => x.Szervezet && x.Tipus == "béke" && x.Evszam >= 1990).ToList().ForEach(x => Console.WriteLine($"\t{x.Evszam}: {x.Keresztnev}"));
+
+            Console.WriteLine("6. feladat:");
+            dijak.Where(x => x.Vezeteknev.Contains("Curie")).ToList().ForEach(x => Console.WriteLine($"\t{x.Evszam}: {x.Nev} ({x.Tipus})"));
+
+            Console.WriteLine("7. feladat:");
+            dijak.GroupBy(x => x.Tipus).ToList().ForEach(x => Console.WriteLine($"\t{x.Key,-16}\t{x.Count()} db"));
+
+            File.WriteAllLines("orvosi.txt", dijak.Where(x => x.Tipus == "orvosi").OrderBy(x => x.Evszam).Select(x => $"{x.Evszam}:{x.Nev}"));
+        }
+    }
+}
